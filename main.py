703758803a41ad6d7da7b0464e92bb177671c2dc
Iskender Mazhitov <isken.majitov1@gmail.com>
2022-05-18 10:06:15 +0600

    [feat] cohort added

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ def developer1method(name: str = "Iskender", email: str = "isken.majitov1@gmail.
                      age = 19, gender = "male", birthdate = "10.10.2002", speciality = "developer", country_of_origin = "KG"): #Iskender
     print(f"Name of developer this method is {name}")
     print(f"Email of developer this method is {email}")
+    print(f"Cohort of developer this method is {cohort}")
