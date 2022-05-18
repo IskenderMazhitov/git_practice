9202013b6d0548fcc1e335d5d640612086659a8f
Iskender Mazhitov <isken.majitov1@gmail.com>
2022-05-18 10:05:31 +0600

    [feat] email added

--- main.py
+++ main.py
@@ -4,3 +4,4 @@
 def developer1method(name: str = "Iskender", email: str = "isken.majitov1@gmail.com", cohort = "Computer Science",
                      age = 19, gender = "male", birthdate = "10.10.2002", speciality = "developer", country_of_origin = "KG"): #Iskender
     print(f"Name of developer this method is {name}")
+    print(f"Email of developer this method is {email}")
