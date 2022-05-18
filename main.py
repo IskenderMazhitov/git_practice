7465aa87b982f0957935c62a75a078fa267e4289
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-05-18 10:19:30 +0600

    country added

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ def developer1method(name: str = "Iskender", email: str = "isken.majitov1@gmail.
 
 def developer3method(name: str = "Tariq", email: str = "axixtariq123@gmail.com", cohort = "Computer Science",
                      age = 20, gender = "male", birthdate = "04.04.2002", speciality = "developer", country_of_origin = "Pakistan"): #Tariq
-    print(f"Name of developer this method is {name}")
-    print(f"Email of developer this method is {email}")
-    print(f"Cohort of developer this method is {cohort}")
-    print(f"Age of developer this method is {age}")
-    print(f"Gender of developer this method is {gender}")
+    print(f"Name is {name}")
+    print(f"Email is {email}")
+    print(f"Cohort is {cohort}")
+    print(f"Age is {age}")
+    print(f"Gender is {gender}")
+    print(f"country is {country_of_origin}")
+    
+    
