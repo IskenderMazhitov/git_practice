d6a7eb224f87fc0163443ac84e108ab417e13608
Tariq Aziz <tariq.aziz_2025@ucentralasia.org>
2022-05-18 10:13:44 +0600

    Name added

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ def developer1method(name: str = "Iskender", email: str = "isken.majitov1@gmail.
     print(f"Age of developer this method is {age}")
     print(f"Gender of developer this method is {gender}")
     print(f"birthdate of dev is {birthdate}")
-    print(f"speciality of dev is {speciality}")
-    print(f"My country is {country_of_origin}")
 
 
 
 def developer3method(name: str = "Tariq", email: str = "axixtariq123@gmail.com", cohort = "Computer Science",
                      age = 20, gender = "male", birthdate = "04.04.2002", speciality = "developer", country_of_origin = "Pakistan"): #Tariq
+    print(f"Name of developer this method is {name}")
