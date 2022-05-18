e162d3318db55f748e12d6070168e6c324694235
Iskender Mazhitov <isken.majitov1@gmail.com>
2022-05-18 10:41:25 +0600

    [feat] adding child class

--- feature1.py
+++ feature1.py
@@ -6,3 +6,13 @@ class Parent:
 
     def __str__(this):
         return f"My name is {this.name}, I'm in {this.age}. My education stage is {this.education}"
+
+
+class Child(Parent):
+    def __init__(this, name, age, surname):
+        super.__init__(name, age)
+        this.surname = surname
+
+    def __str__(this):
+        super.__str__()
+        return f"My surname is {this.surname}"
