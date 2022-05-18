f2a5079433deea2f4e4290683f4b59381971311c
JasurSadiev <goldenwashington159@gmail.com>
2022-05-18 01:08:36 -0400

    Feature3.py is created.

---
+++ feature3.py
@@ -0,0 +1,11 @@
+def developer3method():
+    print("FirstName: Jasurbek")
+    print("Surname: Sadiev")
+    print("Email: jasursadiev2002@gmail.com")
+    print("Cohort: Computer Science")
+    print("Age: 20")
+    print("gender: male")
+    print("BirthDate: 19.04.2002")
+    print("Specialty: Computer Scientist")
+    print("Country: Tajikistan")
+    print("Hello WOrld")
