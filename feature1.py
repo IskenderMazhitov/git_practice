d63d4784f885296b9fe2fea1d5700dfe460b805e
Iskender Mazhitov <isken.majitov1@gmail.com>
2022-05-18 10:36:46 +0600

    [feat] rewrite function of built-in method __str__

--- feature1.py
+++ feature1.py
@@ -3,3 +3,6 @@ class Parent:
         self.name = name
         self.age = age
         self.education = "Bachelor"
+
+    def __str__(self):
+        return f"My name is {self.name}, I'm in {self.age}. My education stage is {self.education}"
