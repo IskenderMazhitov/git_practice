26fdc576503bbedb33406d519bc0bf9a67c31f67
Iskender Mazhitov <isken.majitov1@gmail.com>
2022-05-18 10:38:16 +0600

    [feat] rewrite self(python version of references) to this (java vers)

--- feature1.py
+++ feature1.py
@@ -1,8 +1,8 @@
 class Parent:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.education = "Bachelor"
+    def __init__(this, name, age):
+        this.name = name
+        this.age = age
+        this.education = "Bachelor"
 
-    def __str__(self):
-        return f"My name is {self.name}, I'm in {self.age}. My education stage is {self.education}"
+    def __str__(this):
+        return f"My name is {this.name}, I'm in {this.age}. My education stage is {this.education}"
