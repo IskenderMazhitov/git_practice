2fd2fa697118b25eb71218c6b175cd2410604074
Iskender Mazhitov <isken.majitov1@gmail.com>
2022-05-18 10:02:52 +0600

    [fix] deleted methods

--- main.py
+++ main.py
@@ -4,10 +4,3 @@
 def developer1method(name: str = "Iskender", email: str = "isken.majitov1@gmail.com", cohort = "Computer Science",
                      age = 19, gender = "male", birthdate = "10.10.2002", speciality = "developer", country_of_origin = "KG"): #Iskender
     print(f"Name of developer this method is {name}")
-
-
-def developer2method(): #Jasur
-    pass
-
-def developer3method(): #Tariq
-    pass
