b90834e702aa7f8b86636eda3c8daa3779097832
Iskender Mazhitov <isken.majitov1@gmail.com>
2022-05-18 09:54:40 +0600

    [feat] add methods

---
+++ main.py
@@ -0,0 +1,10 @@
+# developer1method(), developer2method(), developer3method()
+
+def developer1method(): #Iskender
+    pass
+
+def developer2method(): #Jasur
+    pass
+
+def developer3method(): #Tariq
+    pass
